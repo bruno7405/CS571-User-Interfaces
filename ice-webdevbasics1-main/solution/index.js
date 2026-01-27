99b81dd193d80d64b6b74d249057a0c2e3e8ae2d
Bruno Wu <bruno7405@gmail.com>
2026-01-27 09:36:54 -0600

    Example 1-27

---
+++ ice-webdevbasics1-main/solution/index.js
@@ -0,0 +1,15 @@
+// This is where your JS goes!
+
+const REVIEWS = [
+    "A burst of warmth and flavor in every spoonful; simple yet irresistible!",
+    "The perfect blend of spice and comfort, an easy go-to chili recipe.",
+    "Loved the hearty texture and rich taste - a new family favorite!",
+    "Quick, flavorful, and satisfying - this chili hits all the right notes!"
+]
+
+let reviewNum = 0;
+
+document.getElementById("review-btn").addEventListener("click", (e) => {
+    alert(REVIEWS[reviewNum]);
+    reviewNum = (reviewNum + 1) % REVIEWS.length;
+})
