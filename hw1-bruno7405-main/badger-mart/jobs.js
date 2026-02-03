9110a9669be78777cc5ac0af153c01a84c7dc74e
savagebananas <bruno7405@gmail.com>
2026-02-02 21:01:29 -0600

    hw1 finish

--- hw1-bruno7405-main/badger-mart/jobs.js
+++ hw1-bruno7405-main/badger-mart/jobs.js
@@ -1,5 +1,13 @@
 function submitApplication(e) {
-    e.preventDefault(); // You can ignore this; prevents the default form submission!
+  e.preventDefault(); // You can ignore this; prevents the default form submission!
 
-    // TODO: Alert the user of the job that they applied for!
+  // TODO: Alert the user of the job that they applied for!
+  jobs = document.getElementsByName("job");
+  for (const element of jobs) {
+    if (element.checked) {
+      alert("Thank you for applying to be a " + element.value);
+      return;
+    }
+  }
+  alert("Please select a job!");
 }
